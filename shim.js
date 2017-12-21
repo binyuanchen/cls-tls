ed782c138befe5bbf7470218dcc250c97127ee03
Binyuan Chen <binyuanchen@gmail.com>
2017-12-21 15:26:00 -0800

    make shim better

--- shim.js
+++ shim.js
@@ -11,7 +11,13 @@ function slice(args) {
 exports = module.exports = function patchTls(ns) {
     var net = require('net');
     var proto = net && net.Socket && net.Socket.prototype;
-    shimmer.massWrap([proto], ['write', 'on'], function (captured) {
+    /**
+     * wrapping 'connect' is actually not necessary due to its current way of implementation,
+     * however, for future proof, it is wrapped anyway here.
+     *
+     * This shim is for tls/socket client only.
+     */
+    shimmer.massWrap([proto], ['connect', 'write', 'on', 'setTimeout', 'destroy'], function (captured) {
         return function wrapped() {
             var args = slice(arguments);
             var last = args.length - 1;
