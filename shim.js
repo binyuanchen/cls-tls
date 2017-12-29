abb7a89de5f0cdcfc3e2eedb3f4c435edfd9eeb7
Binyuan Chen <binyuanchen@gmail.com>
2017-12-28 19:13:06 -0800

    provide api to patch a single preallocated tls socket (this functionality was previously removed)

--- shim.js
+++ shim.js
@@ -30,14 +30,39 @@ var _wrapCaptured = function (captured, ns) {
 /**
  * Calling this patches the tls module connect method first. It also defines patcher
  * function that patches the tls.TLSSocket instance generated at runtime as part of
- * the call to the tls.connect method.
+ * the call to the tls.connect method. (In another word, if you create socket using
+ * new tls.TLSSocket, those sockets are not patched)
  * @param ns
  */
-var patchTls = function (ns) {
+
+/**
+ * Patching tls module or individual tls socket so they work with cls.
+ *
+ * Parameter 'tlsSocket' is optional. Specify it or not depends on your use cases.
+ *
+ * When tlsSocket is not specified, calling this method register a patcher function that does these:
+ * 1) the tls.connect is patched so it works with the cls namespace,
+ * 2) the tls.TLSSocket instance created as a result of the tls.connect call is also patched so it
+ * works with the cls namespace.
+ *
+ * When tlsSocket is specified, calling this method register a patcher function that patches the
+ * given tlsSocket so  it works with the cls namespace.
+ *
+ * @param ns The cls namespace
+ * @param tlsSocket Optional. When specified, it must be a tls.TLSSocket instance
+ */
+var patchTls = function (ns, tlsSocket) {
 
     var tls = require('tls');
-
-    if (tls && tls.connect) {
+    if (tlsSocket && tlsSocket instanceof tls.TLSSocket) {
+        // patching only a single socket
+        ns.bindEmitter(tlsSocket);
+        shimmer.massWrap([tlsSocket], ['connect', '_writeGeneric'], function (tlsSocket_writeGeneric) {
+            return _wrapCaptured(tlsSocket_writeGeneric, ns);
+        });
+    }
+    else if (tls.connect) {
+        // patching the tls module connect
         shimmer.wrap(tls, 'connect', function (tls_connect) {
             return function () {
                 var args = slice(arguments);
@@ -47,6 +72,8 @@ var patchTls = function (ns) {
                     args[last] = ns.bind(cb);
                 }
                 var tlsSocket = tls_connect.apply(this, args);
+
+                // patches every tls.TLSSocket instances created as result of tls.connect
                 if (tlsSocket && tlsSocket instanceof tls.TLSSocket) {
                     ns.bindEmitter(tlsSocket);
                     shimmer.wrap(tlsSocket, '_writeGeneric', function (tlsSocket_writeGeneric) {
@@ -59,4 +86,6 @@ var patchTls = function (ns) {
     }
 };
 
-exports = module.exports = patchTls;
+exports = module.exports = {
+    patchTls: patchTls
+};
---
+++ test/client_patch_socket_preallocation.tap.js
@@ -0,0 +1,341 @@
+'use strict';
+
+const path = require('path');
+var test = require('tap').test;
+
+const TEST_ITERATIONS = 10;
+
+const kConnectOptions = Symbol('connect-options');
+
+test("cls + tls without patching, using socket creation", function (t) {
+    t.plan(11 * TEST_ITERATIONS);
+
+    var fs = require('fs');
+
+    var tls = require('tls');
+    var cls = require('continuation-local-storage');
+    var ns = cls.createNamespace('test');
+
+    var _send_verify = function (requestData) {
+
+        ns.run(function () {
+            ns.set('requestId', requestData);
+
+            var options = {
+                // defaults
+                rejectUnauthorized: true,
+                ciphers: tls.DEFAULT_CIPHERS,
+                checkServerIdentity: tls.checkServerIdentity,
+                minDHSize: 1024,
+                // added by me
+                ca: [fs.readFileSync(path.join(__dirname, 'server-cert.pem'))],
+                requestOCSP: true,
+                port: 8000,
+                host: 'localhost',
+                // adjusted by framework
+                singleUse: true
+            };
+
+            var securityContext = tls.createSecureContext(options);
+
+            var tlsSocket = new tls.TLSSocket(undefined, {
+                pipe: !!options.path,
+                secureContext: securityContext,
+                isServer: false,
+                requestCert: true,
+                rejectUnauthorized: options.rejectUnauthorized !== false,
+                session: options.session,
+                NPNProtocols: options.NPNProtocols,
+                ALPNProtocols: options.ALPNProtocols,
+                requestOCSP: options.requestOCSP
+            });
+
+            tlsSocket[kConnectOptions] = options;
+
+            // tlsSocket.on('secureConnect', function () {
+            //     console.log('secureConnect');
+            // });
+
+            tlsSocket.on('connect', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']connect');
+                t.notOk(rid, 'without patching, connect cb does not work');
+            });
+
+            tlsSocket.on('lookup', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']lookup');
+                t.equal(rid, requestData, 'without patching, lookup cb still works');
+            });
+
+            // tlsSocket.on('OCSPResponse', function (ocspResponse) {
+            //     console.log('OCSPResponse');
+            // });
+
+            tlsSocket.connect({
+                port: options.port,
+                host: options.host
+            });
+
+            // tlsSocket._releaseControl();
+
+            if (options.session)
+                tlsSocket.setSession(options.session);
+
+            // if (options.servername)
+            //     tlsSocket.setServername(options.servername);
+
+            tlsSocket.once('secure', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']secure');
+                t.notOk(rid, 'without patching, secure cb does not work');
+            });
+
+            tlsSocket.on('close', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']close');
+                t.notOk(rid, 'without patching, close cb does not work');
+            });
+
+            // tlsSocket.on('drain', function () {
+            //     console.log('drain');
+            // });
+
+            tlsSocket.once('end', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']end');
+                t.equal(rid, requestData, 'without patching, end cb still works');
+            });
+
+            tlsSocket.on('finish', function () {
+                // stream.Writable
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']finish');
+                t.notOk(rid, 'without patching, finish cb does not work');
+            });
+
+            // tlsSocket.on('pipe', function () {
+            //     // stream.Writable
+            //     console.log('pipe');
+            // });
+
+            tlsSocket.once('readable', function () {
+                // stream.Readable
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']readable');
+                t.equal(rid, requestData, 'without patching, readable cb still works');
+            });
+
+            tlsSocket.on('data', function (responseData) {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']data = ' + responseData);
+                t.equal(rid, responseData.toString(), 'without patching, data cb still works');
+
+                var renegotiateOptions = {
+                    rejectUnauthorized: true,
+                    requestCert: true
+                };
+
+                tlsSocket.renegotiate(renegotiateOptions, function (err) {
+                    var rid = ns.get('requestId');
+                    // console.log('[requestData=' + requestData + ', rid=' + rid + ']renegotiate');
+                    t.notOk(rid, 'without patching, renegotiate cb does not work');
+
+                    tlsSocket.setTimeout(100);
+                    tlsSocket.on('timeout', function () {
+                        var rid = ns.get('requestId');
+                        // console.log('[requestData=' + requestData + ', rid=' + rid + ']timeout');
+                        t.notOk(rid, 'without patching, timeout cb does not work');
+
+                        tlsSocket.end();
+                    });
+                });
+            });
+
+            tlsSocket.on('error', function (err) {
+                var rid = ns.get('requestId');
+                console.log('[requestData=' + requestData + ', rid=' + rid + ']error = ' + err);
+            });
+
+            tlsSocket.write(requestData, 'utf8', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']write');
+                t.notOk(rid, 'without patching, write cb does not work');
+            });
+        });
+    };
+
+    // run test
+    for (var i = 0; i < TEST_ITERATIONS; i++) {
+        _send_verify('' + i);
+    }
+});
+
+test("cls + tls with patching, using socket creation", function (t) {
+    t.plan(11 * TEST_ITERATIONS);
+
+    var fs = require('fs');
+
+    var tls = require('tls');
+    var cls = require('continuation-local-storage');
+    var ns = cls.createNamespace('test');
+
+    var {patchTls, patchTlsSocket} = require('../shim');
+    // patchTls(ns);
+
+    var _send_verify = function (requestData) {
+
+        ns.run(function () {
+            ns.set('requestId', requestData);
+
+            var options = {
+                // defaults
+                rejectUnauthorized: true,
+                ciphers: tls.DEFAULT_CIPHERS,
+                checkServerIdentity: tls.checkServerIdentity,
+                minDHSize: 1024,
+                // added by me
+                ca: [fs.readFileSync(path.join(__dirname, 'server-cert.pem'))],
+                requestOCSP: true,
+                port: 8000,
+                host: 'localhost',
+                // adjusted by framework
+                singleUse: true
+            };
+
+            var securityContext = tls.createSecureContext(options);
+
+            var tlsSocket = new tls.TLSSocket(undefined, {
+                pipe: !!options.path,
+                secureContext: securityContext,
+                isServer: false,
+                requestCert: true,
+                rejectUnauthorized: options.rejectUnauthorized !== false,
+                session: options.session,
+                NPNProtocols: options.NPNProtocols,
+                ALPNProtocols: options.ALPNProtocols,
+                requestOCSP: options.requestOCSP
+            });
+
+            patchTlsSocket(ns, tlsSocket);
+
+            tlsSocket[kConnectOptions] = options;
+
+            // tlsSocket.on('secureConnect', function () {
+            //     console.log('secureConnect');
+            // });
+
+            tlsSocket.on('connect', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']connect');
+                t.equal(rid, requestData, 'with patching, connect cb works');
+            });
+
+            tlsSocket.on('lookup', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']lookup');
+                t.equal(rid, requestData, 'with patching, lookup cb works');
+            });
+
+            // tlsSocket.on('OCSPResponse', function (ocspResponse) {
+            //     console.log('OCSPResponse');
+            // });
+
+            tlsSocket.connect({
+                port: options.port,
+                host: options.host
+            });
+
+            // tlsSocket._releaseControl();
+
+            if (options.session)
+                tlsSocket.setSession(options.session);
+
+            // if (options.servername)
+            //     tlsSocket.setServername(options.servername);
+
+            tlsSocket.once('secure', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']secure');
+                t.equal(rid, requestData, 'with patching, secure cb works');
+            });
+
+            tlsSocket.on('close', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']close');
+                t.equal(rid, requestData, 'with patching, close cb works');
+            });
+
+            // tlsSocket.on('drain', function () {
+            //     console.log('drain');
+            // });
+
+            tlsSocket.once('end', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']end');
+                t.equal(rid, requestData, 'with patching, end cb works');
+            });
+
+            tlsSocket.on('finish', function () {
+                // stream.Writable
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']finish');
+                t.equal(rid, requestData, 'with patching, finish cb works');
+            });
+
+            // tlsSocket.on('pipe', function () {
+            //     // stream.Writable
+            //     console.log('pipe');
+            // });
+
+            tlsSocket.once('readable', function () {
+                // stream.Readable
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']readable');
+                t.equal(rid, requestData, 'with patching, readable cb works');
+            });
+
+            tlsSocket.on('data', function (responseData) {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']data = ' + responseData);
+                t.equal(rid, responseData.toString(), 'with patching, data cb works');
+
+                var renegotiateOptions = {
+                    rejectUnauthorized: true,
+                    requestCert: true
+                };
+
+                tlsSocket.renegotiate(renegotiateOptions, function (err) {
+                    var rid = ns.get('requestId');
+                    // console.log('[requestData=' + requestData + ', rid=' + rid + ']renegotiate');
+                    t.equal(rid, requestData, 'with patching, renegotiate cb works');
+
+                    tlsSocket.setTimeout(100);
+                    tlsSocket.on('timeout', function () {
+                        var rid = ns.get('requestId');
+                        // console.log('[requestData=' + requestData + ', rid=' + rid + ']timeout');
+                        t.equal(rid, requestData, 'with patching, timeout cb works');
+
+                        tlsSocket.end();
+                    });
+                });
+            });
+
+            tlsSocket.on('error', function (err) {
+                var rid = ns.get('requestId');
+                console.log('[requestData=' + requestData + ', rid=' + rid + ']error = ' + err);
+            });
+
+            tlsSocket.write(requestData, 'utf8', function () {
+                var rid = ns.get('requestId');
+                // console.log('[requestData=' + requestData + ', rid=' + rid + ']write');
+                t.equal(rid, requestData, 'with patching, write cb works');
+            });
+        });
+    };
+
+    // run test
+    for (var i = 0; i < TEST_ITERATIONS; i++) {
+        _send_verify('' + i);
+    }
+});
